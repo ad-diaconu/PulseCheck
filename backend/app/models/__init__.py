8c01d5eb0797d982a28bb585115fd8fca316320f
AI Enthusiast <118270884+ad-diaconu@users.noreply.github.com>
2026-07-29 10:14:13 +0300

    chore(api): setup alembic for database migrations

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,10 @@
+# import all models here to ensure they are registered with the Base for Alembic migrations
+
+from app.db.database import Base
+
+from .monitor import Monitor
+from .ping_history import PingHistory
+from .user import User
+from .workspace import Workspace, WorkspaceUser
+
+# optional: we can now import in other modules to use the models, e.g. from app.models import User
